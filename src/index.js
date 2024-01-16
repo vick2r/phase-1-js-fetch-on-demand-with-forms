30b3c654914a5b7a4bcec92825617a3ef506b1de
Victor Njoroge <victorknjoroge@gmail.com>
2024-01-16 21:21:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (happening) => {
+    happening.preventDefault()
+    const input = document.querySelector('input#searchByID')
+    console.log (input.value)
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/`)
+    .then(response => {
+        if(response.id === input.value) {
+        response.json()} else {
+            console.error ("Invalid Id")
+        }
+  })
+    .then(data => {
+      console.log(data) 
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      title.innerText = data.title
+      summary.innerText = data.summary
+    });
+    })
+
+}
+document.addEventListener('DOMContentLoaded', init)
